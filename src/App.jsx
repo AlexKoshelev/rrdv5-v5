f18cfbb763a551715a7c131e00c8f1c6db05f632
AlexKoshelev <2391382@mail.ru>
2022-11-21 23:20:21 +0300

    router6

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
 import React from "react";
 import HomePage from "./pages/homePage";
-import { Route, Switch, Redirect, Link } from "react-router-dom";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 
 import Users from "./pages/users";
+import UsersList from "./pages/usersList";
+import UserPage from "./pages/userPage";
+import EditPage from "./pages/editPage";
 
 function App() {
+  const users = [
+    { id: 1, name: "user1" },
+    { id: 2, name: "user2" },
+    { id: 3, name: "user3" },
+    { id: 4, name: "user4" },
+    { id: 5, name: "user5" },
+    { id: 6, name: "user6" },
+    { id: 7, name: "user7" },
+    { id: 8, name: "user8" },
+    { id: 9, name: "user9" },
+    { id: 10, name: "user10" },
+  ];
   return (
     <>
       <h1>App layout</h1>
       <Link to="/users">Users List Page</Link>
-      <Switch>
-        <Route path="/users" component={Users} />
-        <Route path="/" component={HomePage} />
-        <Redirect to="/" />
-      </Switch>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="users" element={<Users />}>
+          <Route index element={<UsersList users={users} />} />
+          <Route path=":userId">
+            <Route index element={<UserPage users={users} />} />
+            <Route path="profile" element={<EditPage />} />
+            <Route path="*" element={<Navigate to="" />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </>
   );
 }
--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,40 +1,13 @@
 import React from "react";
-import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
-import User from "./user";
-
-import UsersList from "./usersList";
+import { Link, Outlet } from "react-router-dom";
 
 const Users = () => {
-  const { path } = useRouteMatch();
-
-  const users = [
-    { id: 1, name: "user1" },
-    { id: 2, name: "user2" },
-    { id: 3, name: "user3" },
-    { id: 4, name: "user4" },
-    { id: 5, name: "user5" },
-    { id: 6, name: "user6" },
-    { id: 7, name: "user7" },
-    { id: 8, name: "user8" },
-    { id: 9, name: "user9" },
-    { id: 10, name: "user10" },
-  ];
-
   return (
     <>
       <div>
         <Link to="/">Home Page</Link>
       </div>
-      <Switch>
-        <Route
-          path={path + "/:userId"}
-          render={(props) => <User users={users} {...props} />}
-        />
-        <Route
-          path={path + "/"}
-          render={(props) => <UsersList users={users} {...props} />}
-        />
-      </Switch>
+      <Outlet />
     </>
   );
 };
